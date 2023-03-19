abfb59ade6abc83fbf7e2810ed41bdd6f5255d15
Anton Gusev <uartman@mail.ru>
2023-03-19 16:43:22 +0300

    refactor macroquad snake canvas implementation into a different module

--- macroquad-frontend/src/main.rs
+++ macroquad-frontend/src/main.rs
@@ -6,40 +6,10 @@ use m6502::{
     CPU,
 };
 use macroquad::prelude::*;
+use crate::snake::MCSnakeCanvas;
 
 pub mod harness;
-
-pub struct MCSnakeCanvas {
-    pub image: Image,
-}
-
-impl Default for MCSnakeCanvas {
-    fn default() -> Self {
-        let mut image = Image {
-            bytes: vec![],
-            width: 32,
-            height: 32,
-        };
-        for _ in 0..32 * 32 {
-            image.bytes.push(60);
-            image.bytes.push(60);
-            image.bytes.push(60);
-            image.bytes.push(255);
-        }
-        image.width = 32;
-        image.height = 32;
-        Self { image }
-    }
-}
-
-impl SnakeCanvas for MCSnakeCanvas {
-    fn write_pixel(&mut self, at: usize, colors: (u8, u8, u8, u8)) {
-        self.image.bytes[at * 4] = colors.0;
-        self.image.bytes[at * 4 + 1] = colors.1;
-        self.image.bytes[at * 4 + 2] = colors.2;
-        self.image.bytes[at * 4 + 3] = colors.3;
-    }
-}
+pub mod snake;
 
 #[macroquad::main("6502 Emulator")]
 async fn main() {
---
+++ macroquad-frontend/src/snake.rs
@@ -0,0 +1,34 @@
+use macroquad::prelude::Image;
+use m6502::bus::SnakeCanvas;
+
+pub struct MCSnakeCanvas {
+    pub image: Image,
+}
+
+impl Default for MCSnakeCanvas {
+    fn default() -> Self {
+        let mut image = Image {
+            bytes: vec![],
+            width: 32,
+            height: 32,
+        };
+        for _ in 0..32 * 32 {
+            image.bytes.push(60);
+            image.bytes.push(60);
+            image.bytes.push(60);
+            image.bytes.push(255);
+        }
+        image.width = 32;
+        image.height = 32;
+        Self { image }
+    }
+}
+
+impl SnakeCanvas for MCSnakeCanvas {
+    fn write_pixel(&mut self, at: usize, colors: (u8, u8, u8, u8)) {
+        self.image.bytes[at * 4] = colors.0;
+        self.image.bytes[at * 4 + 1] = colors.1;
+        self.image.bytes[at * 4 + 2] = colors.2;
+        self.image.bytes[at * 4 + 3] = colors.3;
+    }
+}
